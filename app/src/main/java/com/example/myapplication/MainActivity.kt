0e9dec7564d8b5a589eb0d6fdb14cd21e1ed3210
maulida08 <maulida.ir.08@gmail.com>
2022-03-08 19:34:23 +0700

    Add some comments

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,5 +7,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Hallo nama saya Ida
+        //Boleh kenalan nggak??
+        //Nama kamu siapa
+
+        //1
+        //50
+        //..
+        //100
+        //Stash
+        //
+        //100 Baris Code
     }
+//    fun someMethod(){
+//        //Some statements
+//    }
 }
